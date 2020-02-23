ba6e0d67af2a4831a25f2f63779b91053c5f731a
unknown <qweas46@naver.com>
2020-02-23 20:19:41 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+//스타일 변수 지정
+const styles = theme => ({
+  root: {
+    width:'100%',//너비가 100퍼센트
+    marginTop: theme.spacing.unit*3, // 여백을 3의 가중치만큼 줌
+    overflowX: "auto" // 전체 바깥쪽에 해당하는 루트는 오버 플로 발생을 만듬
+  },
+  table:{
+    minWidth: 1080 // 창을 줄여도 1080 픽셀 만큼은 무조건 만들게 해줌
+  }
+})
 
 const customers = [{
   'id': 1,
@@ -19,24 +38,38 @@ const customers = [{
   'job': '프로그래머'
 },
 {
-  'id': 1,
+  'id': 3,
   'image': 'https://placeimg.com/64/64/3',
   'name': '이순신',
   'birthday': '921205',
-  'gender': '남성',
+  'gender': '남자',
   'job': '디자이너'
 }
 ]
 
 class App extends Component {
   render() {
+    const {classes} = this.props;
     return (//pros<프롭스>라는 개념을 이용해서 한명의 정보를 출력한다.
-      <div>
+      <Paper className={classes.root}>
         { //map 함수를 이용해서 여러개를 일일히 호출할 필요가 없어졌다.
-          customers.map(c => {//map을 이용하면 키값이 필요하다.
-            return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})
-        }
-      </div>
+          <Table class={classes.table}>
+          <TableHead>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {//map을 이용하면 키값이 필요하다.
+            return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+          </TableBody>
+          </Table>
+       }
+      </Paper>
+      //// Paper은 감싸주기 위해서 사용한다.
       //  <Customer
       //   id={customers[0].id}
       //   image={customers[0].image}
@@ -65,7 +98,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
 
 
 // import React from 'react';
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,36 +1,19 @@
 import React from 'react';//리액트 라이브러리를 사용하기 위해 임포트
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 /*React.Component는 라이브러리이자 클래스이다. 상속을 이용함*/
 class Customer extends React.Component {
     render() { //렌더는 항상 수행 되는 것으로써, 커스터머라는 클래스를 실제로 그리고자 할때 사용
         return (//렌더라는 함수는 항상 리턴이라는 것을 이용해서 반환을 해줘야 한다.
-           <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-           </div>
-        )
-    }
-}
-
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
+            <TableRow> 
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>//사용자를 한줄에 담기 위해서 사용
         )
     }
 }
